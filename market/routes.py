c580ccd91c24a1b83c6984eae41d05effc774edd
Harsh <harshgopalani05@gmail.com>
2021-12-23 10:13:35 +0530

    first commit

---
+++ market/routes.py
@@ -0,0 +1,77 @@
+from market import app
+from flask import render_template,redirect,url_for,flash,request
+from market.models import Item,User
+from market.forms import LoginForm, RegisterForm, PurchaseForm, SellForm
+from market import db
+from flask_login import login_user,logout_user,login_required,current_user
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+  return render_template('home.html')
+@app.route('/market',methods=['GET','POST'])
+@login_required
+def market_page():
+  items = Item.query.all()
+  purchase_form=PurchaseForm()
+  selling_form=SellForm()
+  if request.method=="POST":
+      purchased_item=request.form.get('purchased_item')
+      p_item_object=Item.query.filter_by(name=purchased_item).first()
+      if p_item_object:
+         if current_user.can_purchase(p_item_object):
+            p_item_object.buy(current_user)
+         else:
+            flash(f'Not enough money to buy {p_item_object.name}',category='danger') 
+      
+      sold_item=request.form.get('sold_item')
+      s_item_object=Item.query.filter_by(name=sold_item).first()
+      if s_item_object:
+         if current_user.can_sell(s_item_object):
+            s_item_object.sell(current_user)
+         else:
+            flash(f'Something went wrong',category='danger')   
+      
+      return redirect(url_for('market_page'))
+     
+      
+  if request.method=="GET":       
+      items=Item.query.filter_by(owner=None)   
+      owned_items=Item.query.filter_by(owner=current_user.id)
+      return render_template('market.html',items=items,purchase_form=purchase_form,owned_items=owned_items,selling_form=selling_form)  
+
+@app.route('/register',methods=['GET','POST'])
+def register_page():
+  form=RegisterForm()
+  if form.validate_on_submit():
+      user_to_create=User(username=form.username.data,
+                         email_address=form.email_address.data,
+                         password=form.password1.data)
+      db.session.add(user_to_create)
+      db.session.commit()
+      login_user(user_to_create)
+      flash(f'Account created, Logged In as {user_to_create.username}',category='success')
+      return redirect(url_for('market_page')) 
+  if form.errors != {}:
+     for err_msg in form.errors.values():
+       flash(f'Error:{err_msg}',category='danger')   
+  return render_template('register.html',form=form)
+
+@app.route('/login',methods=['GET','POST'])
+def login_page():
+    form=LoginForm()
+    if form.validate_on_submit():
+       attempted_user=User.query.filter_by(username=form.username.data).first()
+       if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+          login_user(attempted_user)
+          flash(f'You are logged in as: {attempted_user.username}',category='success')
+          return redirect(url_for('market_page')) 
+       else:
+           flash('Username and password are not matched',category='danger')   
+    return render_template('login.html',form=form)  
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("Logged Out",category='info')
+    return redirect(url_for("home_page"))    
